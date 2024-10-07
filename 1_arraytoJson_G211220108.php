6dd21b23cec77dab3e5299e7af36c3b336032667
Riyanputraa <136705264+Riyanputraa@users.noreply.github.com>
2024-10-07 10:22:11 +0700

    Add files via upload

---
+++ 1_arraytoJson_G211220108.php
@@ -0,0 +1,13 @@
+<?php
+
+$data = array(
+    "nama" => "Muhammad Riyan Febriansyah Putra",
+    "usia" => 20,
+    "pekerjaan" => "Mahasiswa",
+    "NIM" => "G.211.22.0108"
+);
+
+$json_data = json_encode($data);
+
+echo $json_data;
+?>
